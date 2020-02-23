da9ac223ab89eaf9782be823422b3cbd46822876
divyang-mittal <divyangmittal44@gmail.com>
2020-02-24 00:41:01 +0530

    final working code

--- Assgn5/simProxy.c
+++ Assgn5/simProxy.c
@@ -14,7 +14,7 @@
 #include <netdb.h>
 #include <errno.h>
 
-const int MAX_SIZE = 1024;
+const int MAX_SIZE = 100000;
 const int MAX_CON = 1000;
 
 #define STDIN 0
@@ -85,7 +85,7 @@ int main(int argc,char *argv[])
 	listen ( tcp_socket_fd, MAX_CON); 			// listen to at max MAX_SIZE connections
 	
 	printf( " Server Running....\n");
-
+	printf("Proxy Running on port %d. Forwarding all connections to %s:%s \n",(int)(ntohs(server_addr.sin_port)),argv[2],argv[3]);
 	int insti_fd[MAX_CON];	// decalre array to store fds of outgoing connections to intitute server [same]
 	int browser_fd[MAX_CON];	// declare fds for incoming browser connections
 	fd_set fd_read;			// set of fds
@@ -97,7 +97,6 @@ int main(int argc,char *argv[])
 		insti_fd[i]= -1;
 		browser_fd[i]=-1;
 	}
-		printf("Hello from the other side\n");
 
 	
 	int max =0 , conn_count =0 ;		// conn_count is the number of connections till now
@@ -107,10 +106,7 @@ int main(int argc,char *argv[])
 
 		FD_SET( STDIN, &fd_read);
 		FD_SET( tcp_socket_fd, &fd_read);
-		printf("Hello from the other side\n");
-
 		max = tcp_socket_fd;
-		printf("12\n");
 		for(int i = 0; i < MAX_CON ; i++)
 		{
 			FD_SET( insti_fd[i], &fd_read);
@@ -123,10 +119,8 @@ int main(int argc,char *argv[])
 		}
 
 		max++;				// max file descriptor
-		printf("2\n");
 		if(select(max, &fd_read, &fd_send, NULL, NULL)<=0)
 			perror("Select failed");
-		printf("3\n");
 		if(FD_ISSET(STDIN, &fd_read))
 		{
 			scanf("%s", input);
@@ -135,13 +129,15 @@ int main(int argc,char *argv[])
 
 		if(FD_ISSET(tcp_socket_fd, &fd_read) && conn_count < MAX_CON)
 		{
-			printf("9\n");
 			browser_fd[conn_count] = accept( tcp_socket_fd, (struct sockaddr *)&client_addr, &clilen );
 			if( browser_fd[ conn_count] == -1 )
 			{		
 				perror("ACCEPT FAILED");
 				// exit(1);
 			}
+			char str[100]; 
+            inet_ntop(AF_INET, &(client_addr.sin_addr), str, 100);
+			printf("Connection accepted from %s:%d \n",str,(int)ntohs(client_addr.sin_port));
 
 			// if browser connection is established
 			// establish corresponding new connection to inti_fd
@@ -153,30 +149,24 @@ int main(int argc,char *argv[])
 		        perror("Could not make non-blocking socket\n");
 		        exit(EXIT_FAILURE);
 		    }
-		    printf("10\n");
 
 		    if (setsockopt(insti_fd[conn_count], SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
 		    {
 		        perror("Setsockopt failure for socket in\n");
 		    }
 
-			if(connect( insti_fd[conn_count], (struct sockaddr *)&intiserver, sizeof(intiserver) )<0)
+			if(connect( insti_fd[conn_count], (struct sockaddr *)&intiserver, sizeof(intiserver) )<0 && errno != EINPROGRESS)
 				perror("INSTI SERVER CONNECTION FAILED");
-			printf("4\n");
 			conn_count++;
 
 		}
 
 		for( int i = 0; i< conn_count ; i++)
-		{  // iterate over already establised connecitons	
+		{ 
+			 // iterate over already establised connecitons	
 			if(FD_ISSET(browser_fd[i], &fd_read)&&FD_ISSET(insti_fd[i],&fd_send))
 			{
-				/* 
-					do fcntl recv, send to inti_fd[i]
-				*/
-
 				// recv from browser i
-				printf("8\n");
 				char buffer[MAX_SIZE];
 				for( int i = 0; i<MAX_SIZE; i++)buffer[i] = '\0';			// intialize buffer to null
 				int msg_len = read( browser_fd[i], buffer, sizeof(buffer));	// recv from browser i
@@ -184,105 +174,30 @@ int main(int argc,char *argv[])
 					perror("Error: Reading failed from browser");
 				if(msg_len==0)
 				{
-					perror("Empty Message");
+					// perror("Empty Message");
 					continue;
 				}
-				// while( msg_len >= 0)
-				// {
-					
-				// 	// send to inti server i 
-					
-				// 	// for( int i = 0; i<MAX_SIZE; i++)buffer[i] = '\0';			// intialize buffer to null
-				// 	if(msg_len == 0 )
-				// 	{											// if connection closed
-				// 		printf("conneciton Closed on browser : %d\n", i);
-				// 		break;
-				// 	}
-
-				// 	for( int i = 0; i<MAX_SIZE; i++)buffer[i] = '\0';			// intialize buffer to null
-
-				// 	msg_len = recv( browser_fd[i], buffer, sizeof(buffer), 0);
-				// }
 
 				send( insti_fd[i], buffer, msg_len, 0);				// send to inti connection
-				// {
-				// 	perror("SENDING FAILED TO INSTITUTE");
-				// }
 				if(errno == EPIPE)
 					continue;
-				printf("5\n");
 			}
 
 			if(FD_ISSET(browser_fd[i], &fd_send)&&FD_ISSET(insti_fd[i],&fd_read))
 			{
-				/* 
-					do fcntl recv, send to browser_fd[i]
-				*/
-				printf("7\n");
 				char buffer[MAX_SIZE];
 				for( int i = 0; i<MAX_SIZE; i++)buffer[i] = '\0';			// intialize buffer to null
-				// int msg_len = read( insti_fd[i], buffer, sizeof(buffer));	// recv from browser i
-				int msg_len;
-				// if(msg_len < 0 )
-				// 	perror("Error: Reading failed from institue");
-				// printf("11\n");
-				// if(msg_len==0)
-				// {
-				// 	perror("Empty Message");
-				// 	continue;
-				// }
-				printf("%d %d\n",conn_count, i);
-				msg_len = recv( browser_fd[i], buffer, sizeof(buffer), MSG_DONTWAIT);
-				printf("Bolo AZAADI\n");
-				if(errno == EWOULDBLOCK||errno == EAGAIN)
-				{
-					perror("Recieve ERROR: ");
-					continue;
-				}
-				if(msg_len==0)
-				{
-					perror("Empty Message");
-					continue;
-				}
+				int msg_len = read( insti_fd[i], buffer, sizeof(buffer));	// recv from browser i
 				if(msg_len < 0 )
 					perror("Error: Reading failed from institue");
-
-				while( msg_len >= 0)
+				if(msg_len==0)
 				{
-					
-					// send to inti server i 
-					
-					// for( int i = 0; i<MAX_SIZE; i++)buffer[i] = '\0';			// intialize buffer to null
-					if(msg_len == 0 )
-					{											// if connection closed
-						printf("conneciton Closed on browser : %d\n", i);
-						break;
-					}
-					send( browser_fd[i], buffer, msg_len, 0);
-					if(errno == EPIPE)
-						continue;
-					for( int i = 0; i<MAX_SIZE; i++)buffer[i] = '\0';			// intialize buffer to null
-
-					msg_len = recv( browser_fd[i], buffer, sizeof(buffer), 0);
-					if(errno == EWOULDBLOCK||errno == EAGAIN)
-					{
-						perror("Recieve ERROR: ");
-						break;
-					}
-					if(msg_len==0)
-					{
-						perror("Empty Message");
-						break;
-					}
-					if(msg_len < 0 )
-						perror("Error: Reading failed from institue");
-
+					// perror("Empty Message");
+					continue;
 				}
-
-				// send( browser_fd[i], buffer, msg_len, 0);
-				// if(errno == EPIPE)
-				// 	continue;
-				printf("6\n");
+				send( browser_fd[i], buffer, msg_len, 0);
+				if(errno == EPIPE)
+					continue;
 			}
 
 		}
