72ecf48ff52ddaedff7c9a4d56407843c37d0c46
Divyang Mittal <37113922+divyang-mittal@users.noreply.github.com>
2020-03-25 17:48:10 +0530

    Update user2.c

--- Assgn7/user2.c
+++ Assgn7/user2.c
@@ -12,12 +12,12 @@
 
 int main(int argc,  char **argv ) { 
   
-    struct sockaddr_in m1_addr, m2_addr; 
+    struct sockaddr_in addr_1, addr_2; 
     socklen_t len; 
     char buffer[MAXLINE] ;
 
-    memset(&m1_addr, 0, sizeof(m1_addr)); 
-    memset(&m2_addr, 0, sizeof(m2_addr)); 
+    memset(&addr_1, 0, sizeof(addr_1)); 
+    memset(&addr_2, 0, sizeof(addr_2)); 
 
     // Creating socket file descriptor 
     int sockfd = r_socket(AF_INET, SOCK_MRP, 0);
@@ -26,24 +26,22 @@ int main(int argc,  char **argv ) {
         exit(EXIT_FAILURE); 
     } 
 
-    m2_addr.sin_family = AF_INET;
-    m2_addr.sin_port = htons(5000 + 2 * ROLLNO);
-    m2_addr.sin_addr.s_addr = INADDR_ANY; 
+    addr_2.sin_addr.s_addr = INADDR_ANY; 
+    addr_2.sin_family = AF_INET;
+    addr_2.sin_port = htons(5000 + 2 * ROLLNO);
+   
 
-    if (r_bind(sockfd, (const struct sockaddr *)&m2_addr, sizeof(m2_addr)) < 0 ) { 
+    if (r_bind(sockfd, (const struct sockaddr *)&addr_2, sizeof(addr_2)) < 0 ) { 
         perror("bind failed"); 
         r_close(sockfd);
         exit(EXIT_FAILURE); 
     }
   
-    len = sizeof(m1_addr);
-    printf("Hi\n");
+    len = sizeof(addr_1);
     for(int i = 0;i<MAXLINE;i++){  
-        int n = r_recvfrom(sockfd, (char *)buffer, MAXLINE, 0,  ( struct sockaddr *) &m1_addr, &len);
+        int n = r_recvfrom(sockfd, (char *)buffer, MAXLINE, 0,  ( struct sockaddr *) &addr_1, &len);
         printf("%s\n", buffer); 
-        // fflush(stdout);
     }
-    // printf("\n");
-    r_close(sockfd); 
+    r_close(sockfd);
     return 0; 
 } 
